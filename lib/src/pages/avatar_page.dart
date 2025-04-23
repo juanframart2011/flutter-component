663dfb8ac819e1c1fe962a63fca02061ee0797c7
juanframart2011 <juanframart2011@gmail.com>
2025-04-22 22:13:06 -0600

    inicio de avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,11 +8,33 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: const Text('SL'),
+            ),
+          ),
+          const SizedBox(
+            width: 10.0,
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text( 'SL'),
+            ),
+          ),
+        ],
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('Mostrar Alerta'),
-          onPressed: () => '',
+        child: FadeInImage(
+          image: const NetworkImage('https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2014/07/363612-avance-naruto-shippuden-uns-revolution.jpg'),
+          placeholder: const AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: const Duration(milliseconds: 200),
         ),
       ),
       floatingActionButton: FloatingActionButton(
